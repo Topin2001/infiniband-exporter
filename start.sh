71d8227eb47f0a9b5e1869b4d4fa5e12b225cb75
tgalpin <thomas.galpin@cern.ch>
2023-06-08 18:15:01 +0200

    added a transceiver state detector

--- start.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-/usr/bin/python3 /home/tgalpin/running/info-exporter/info-exporter.py --port 9686 --node-name-map /home/tgalpin/running/info-exporter/guid_2_name.txt --can-reset-counter
-
-/usr/bin/python3 /home/tgalpin/running/info-exporter/info-exporter.py --phy --link_state --asic_temp --node-name-map /home/tgalpin/running/info-exporter/guid_2_name.txt
